5f7bcac2e7281c8a8008f0f602d5ab9f5393fddb
Yazan Tarifi <yazantarifi989@gmail.com>
2024-01-22 22:24:34 +0300

    Add Project Files

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/yazantarifi/Code/Tapper/documentation/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/yazantarifi/Code/Tapper/documentation/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/yazantarifi/Code/Tapper/documentation/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/yazantarifi/Code/Tapper/documentation/src/css/custom.css"),
+];
